2de370ae8ef722e087e3c0e891337fa76fd8b2ec
Lazkin Youssuf <lazkin.youssuf96@yahoo.com>
2023-01-31 14:17:42 +0100

    Routing fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { CataloguePage } from './pages/catalogue/catalogue.page';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    TrainerPage,
+    CataloguePage
   ],
   imports: [
     BrowserModule,
