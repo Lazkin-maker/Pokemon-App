099e06d1640fac743b684358f6042200522a9b54
Rubin Barclay <rubin.barclay@se.experis.com>
2023-02-03 13:07:40 +0100

    moved public urls to a enum object

---
+++ src/app/enums/public-urls.enum.ts
@@ -0,0 +1,4 @@
+export enum PublicURLs {
+    pokemonAvatars = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",
+    pokeAPI = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+}
